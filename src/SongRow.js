d77256c0c3b9e54880c379065ec5ea1eb2552d85
Manish <you@example.com>
2023-06-18 20:23:26 +0530

    Completed V1

--- src/SongRow.js
+++ src/SongRow.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import './SongRow.css'
-function SongRow({track}) {
-  return (
-    <div className='songRow'>
-        <img src={track.album.images[0].url} className='songRow_Album'></img>
-      
-        <div className='songRow_Info'></div>
-        <h1>{track.name}</h1>
-        
-      <p>
-        {track.artists.map(artist=>artist.name).join(",")}
-        {track.album.name}
-      </p>
+function SongRow({ track }) {
+    return (
+        <div className='songRow'>
+            <img src={track.album.images[0].url} className='songRow_Album'></img>
+
+            <div className='songRow_Info'>
+                <h3>{track.name}</h3>
+
+                <p>  {track.artists.map(artist => artist.name).join(",")} {track.album.name} </p>
+            </div>
+
         </div>
-  )
+    )
 }
 
 export default SongRow
