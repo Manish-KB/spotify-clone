78f2174322c6e470b6923d71773801e47334c6a2
Manish <you@example.com>
2023-06-18 22:21:22 +0530

    Completed V1.2

--- src/Footer.js
+++ src/Footer.js
@@ -17,30 +17,34 @@ function Footer() {
     return (
         <div className='footer'>
             <div className='footer_left'>
-               <img src='https://upload.wikimedia.org/wikipedia/en/3/37/Treat_You_Better_%28Official_Single_Cover%29_by_Shawn_Mendes.png'alt='' className='footer_albumLogo'/>
-               <div className="footer_songInfo">
-                <h4>Song</h4>
-                <p>Artist</p>
-               </div>
+                <img src='https://upload.wikimedia.org/wikipedia/en/3/37/Treat_You_Better_%28Official_Single_Cover%29_by_Shawn_Mendes.png' alt='' className='footer_albumLogo' />
+                <div className="footer_songInfo">
+                    <h4>Song</h4>
+                    <p>Artist</p>
+                </div>
 
 
             </div>
             <div className='footer_center'>
-                <ShuffleIcon className='footer_icon' />
-                <SkipPreviousIcon className='footer_icon' />
-                <PlayCircleFilledOutlinedIcon className="footer_icon play_btn" fontSize="large" />
-                <SkipNextIcon className='footer_icon' />
-                <RepeatRoundedIcon className='footer_icon' />
-
+                <div className='center_icons'>
+                    <ShuffleIcon className='footer_icon' />
+                    <SkipPreviousIcon className='footer_icon' />
+                    <div className="play_btn_container">
+                    <PlayCircleFilledOutlinedIcon className="footer_icon play_btn" fontSize="large" />
+                    </div>
+                    <SkipNextIcon className='footer_icon' />
+                    <RepeatRoundedIcon className='footer_icon' />
+                </div>
             </div>
 
             <div className='footer_right'>
-                <QueueMusicIcon className='footer_icon'/>
+                <QueueMusicIcon className='footer_icon' />
                 <Box sx={{ width: 200 }}>
                     <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
                         <VolumeDown />
                         {/* <Slider aria-label="Volume" value="50" onChange="20" color="secondary"/> */}
-                        <Slider aria-label="Volume" value="50"   />
+                        {/* <Slider aria-label="Volume" value="50"   /> */}
+                        <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
                         <VolumeUp />
                     </Stack>
 
